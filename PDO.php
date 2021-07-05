59845466b457e4e0ff86808445ffc1ed2d60e745
lakshitha perera <lakshithak@projectxspace.com>
2021-07-05 10:15:48 +0530

    deploy

--- PDO.php
+++ PDO.php
@@ -1,9 +1,9 @@
 <?php
 $servername = "us-cdbr-east-04.cleardb.com";
 $username = 'b20d65d4f653dc';
-$password = '7a987524';
+$password = 'ad3e02fe241d510';
 
-    $connect = new PDO("mysql:host=$servername;dbname=heroku_360407db4f1262", $username, $password);
+    $connect = new PDO("mysql:host=$servername;dbname=heroku_360407db4f12625", $username, $password);
     // set the PDO error mode to exception
     $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
  
--- connect.php
+++ connect.php
@@ -1,17 +1,21 @@
 <?php
-error_reporting(0);
+// $hostname = 'localhost';
+// $username = 'root';
+// $password = '';
+// $dbname = 'dlinerentcar_db';
 
+//heroku connect
 $hostname = 'us-cdbr-east-04.cleardb.com';
 $username = 'b20d65d4f653dc';
-$password = '7a987524';
-$dbname = 'heroku_360407db4f1262';
+$password = 'ad3e02fe241d510';
+$dbname = 'heroku_360407db4f12625';
 
-$con = new mysqli($hostname,$username,$password,$dbname);
+$con = new mysqli($hostname, $username, $password, $dbname);
 
-if (mysqli_connect_errno())
-  {
+if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
+}
+
 
 
 ?>
