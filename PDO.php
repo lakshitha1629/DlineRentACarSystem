c4babc55808e309196fc6b25325d9c6e0d2296cf
lakshitha perera <lakshithak@projectxspace.com>
2021-07-05 10:07:44 +0530

    make it better

--- PDO.php
+++ PDO.php
@@ -1,9 +1,9 @@
 <?php
-$servername = "localhost";
-$username = 'root';
-$password = '';
+$servername = "us-cdbr-east-04.cleardb.com";
+$username = 'b20d65d4f653dc';
+$password = '7a987524';
 
-    $connect = new PDO("mysql:host=$servername;dbname=dlinerentcar_db", $username, $password);
+    $connect = new PDO("mysql:host=$servername;dbname=heroku_360407db4f1262", $username, $password);
     // set the PDO error mode to exception
     $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
  
--- connect.php
+++ connect.php
@@ -1,10 +1,10 @@
 <?php
 error_reporting(0);
 
-$hostname = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'dlinerentcar_db';
+$hostname = 'us-cdbr-east-04.cleardb.com';
+$username = 'b20d65d4f653dc';
+$password = '7a987524';
+$dbname = 'heroku_360407db4f1262';
 
 $con = new mysqli($hostname,$username,$password,$dbname);
 
